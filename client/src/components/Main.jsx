1ab9e8f9134f7baf91833f3b72715276c3ccb4b0
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-08-31 19:53:02 +0300

    modify the layout

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,3 +1,5 @@
+import {Telescope, Presentation,Contact, Activity,NotebookTabs, Award} from "lucide-react"
+
 const Main = () => {
     return (
         <>
@@ -12,64 +14,79 @@ const Main = () => {
                     </p>
             </div>
             </section>
-            <section className="call-to-action">
+            <section className="bg-gray-400 py-[4rem] text-center">
                 <div   className="
                 w-[90%] max-w-[700px] mx-auto flex justify-center items-center flex-col py-[2em] px-[0.4em]" >
-                    <a href="#"   className="p-[0.5em] text-[1rem] rounded-[26px] 
-  bg-[linear-gradient(37deg,#171111_5%_-6%,#16168b_34%,blue,#455ae2,transparent)] 
-  uppercase text-white font-medium mb-[0.7em]">
-                        join the move
+                    <a href="#"   className="p-[.6em] text-[1rem] rounded-[26px] bg-amber-800 uppercase text-white font-medium mb-[0.7em]">
+                        join the move &gt;
                     </a>
-                    <p className="text-[0.4rem] font-light">
+                    <p className="text-[1.2rem] font-extralight text-white">
                         become part of the revolution that is shaping the africa of the future
                     </p>
                 </div>
             </section>
-            <section>
-                <h2 className="text-center
-                    mb-[0.2em] text-2xl font-bold
-                ">
-                    our gallery
-                </h2>
-                <div className=" w-full md:max-w-[700px]
-                    flex justify-evenly items-center py-3
-                ">
-                    <tile  className="
-                        py-1 px-2.5 rounded-2xl bg-gray-200 flex justify-center items-center
-                    ">
-                        <img src="#"    alt="" />
-                        <div className="title"></div>
-                        <div className="p">
-                            description
-                        </div>
-                    </tile>
-                    <tile>
-                        <img src="#"    alt="" />
-                        <div className="title"></div>
-                        <div className="p">
-                            description
-                        </div>
-                    </tile>
-                    <tile>
-                        <img src="#"    alt="" />
-                        <div className="title"></div>
-                        <div className="p">
-                            description
-                        </div>
-                    </tile>
+            <section className="mt-[2rem] mb-[3em]">
+                <div className="text-center w-[90%] sm:max-w-[700px] mb-[3em] p-[0.2em] mx-auto">
+                    <h2 className="text-[2rem] font-bold mb-1">why iTech club?</h2>
+                    <p className="font-extralight">Discober the benefits of being part of out thriving tech community at Zetech University</p>
                 </div>
+                <div className="flex w-[80%]  text-center mx-auto md:justify-evenly items-center gap-6 flex-row flex-wrap ">
+                    <div className="flex justify-center w-full md:max-w-[200px] py-[1.2em]  px-[.8em] items-center flex-col  rounded-3xl bg-gray-100 hover:bg-gray-300 p-[0.2em]">
+                        <div className="w-[80px] h-[80px] mb-[2em] overflow-hidden rounded-full flex justify-center items-center object-cover border-[2px] border-none  text-red-500 "> 
+                        <Telescope />
+                        </div>
+                        <h3 className="mb-[0.3em] font-bold text-[1.2rem]">Focused Learning</h3>
+                        <p className="font-light mb-[2rem]">Specialize tracks in various tech dormains to help you  master your chosen field</p>   
+                    </div>    
+                    <div className="flex justify-center w-full md:max-w-[200px] py-[1.2em]  px-[.8em] items-center flex-col  rounded-3xl bg-gray-100 hover:bg-gray-300 p-[0.2em]">
+                        <div className="w-[80px] h-[80px] mb-[2em] overflow-hidden rounded-full flex justify-center items-center object-cover border-[2px] border-none text-amber-400 ">
+                        <Contact />    
+                        </div> 
+                        <h3 className="mb-[0.3em] font-bold text-[1.2rem]">Mentorship</h3>
+                        <p className="font-light mb-[2rem]">Get guidance from experienced mentors and industry professionals</p>   
+                    </div>   
+                     <div className="flex justify-center w-full md:max-w-[200px] py-[1.2em]  px-[.8em] items-center flex-col  rounded-3xl bg-gray-100 hover:bg-gray-300 p-[0.2em]">
+                        <div className="w-[80px] h-[80px] mb-[2em] overflow-hidden rounded-full flex justify-center items-center object-cover border-[2px] border-none text-green-500 ">
+                           <Presentation />
+                            </div> 
+                        <h3 className="mb-[0.3em] font-bold text-[1.2rem]">Industry Projects</h3>
+                        <p className="font-light mb-[2rem]">Work on real-world projects and build a profesional portfolio</p>   
+                    </div>   
+                    <div className="flex justify-center w-full md:max-w-[200px] py-[1.2em]  px-[.8em] items-center flex-col  rounded-3xl bg-gray-100 hover:bg-gray-300 p-[0.2em]">
+                        <div className="w-[80px] h-[80px] mb-[2em] overflow-hidden rounded-full flex justify-center items-center object-cover border-[2px] border-none text-blue-700 ">
+                            <NotebookTabs />
+                        </div>
+                        <h3 className="mb-[0.3em] font-bold text-[1.2rem]">Networking</h3>
+                        <p className="font-light mb-[2rem]">Connect with like-minded peers and industry leaders</p>   
+                    </div>   
+                    <div className="flex justify-center w-full md:max-w-[200px] py-[1.2em]  px-[.8em] items-center flex-col  rounded-3xl bg-gray-100 hover:bg-gray-300 p-[0.2em]">
+                        <div className="w-[80px] h-[80px] mb-[2em] overflow-hidden rounded-full flex justify-center items-center object-cover border-[2px] border-none text-red-900">
+                            <Award />
+                        </div>
+                        <h3 className="mb-[0.3em] font-bold text-[1.2rem]">Competitions</h3>
+                        <p className="font-light mb-[2rem]">Participate in hackathons and coding competitions</p>   
+                    </div>   
+                    <div className="flex justify-center w-full md:max-w-[200px] py-[1.2em]  px-[.8em] items-center flex-col  rounded-3xl bg-gray-100 hover:bg-gray-300 p-[0.2em]">
+                        <div className="w-[80px] h-[80px] mb-[2em] overflow-hidden rounded-full flex justify-center items-center object-cover border-[2px] border-none text-fuchsia-500">
+                            <Activity />
+                        </div>
+                        <h3 className="mb-[0.3em] font-bold text-[1.2rem]">Hands-on activities</h3>
+                        <p className="font-light mb-[2rem]">Learn by doing  avoid toutorial hell .. </p>   
+                    </div>   
+                </div>              
             </section>
             <section className="py-[0.3em] px-[0.2em]">
                 <div className="flex-col
-                    flex justify-center items-center  w-[90%] md:max-w-[700px]  mx-auto
+                    flex justify-center pt-[4em] items-center text-center  w-[90%] md:max-w-[700px]  mx-auto
                 ">
-                    <h2 className="align-center mb-[ 0.2rem]   text-[2rem] ">
+                    <h2 className="align-center mb-[ 0.2rem] font-extrabold text-4xl mb-5">
                         convinced!
                     </h2>
                     <div   className="font-[.78rem] mb-[1.2em]
                     ">join the band wagon and let's build together</div>
                     <a href="#"   className="
-                    py-[.2em] px-[.4em] rounded-2xl border-2 w-[20%] text-center mb-1
+                    p-[.6em] rounded-2xl bg-amber-700 w-[20%] text-center
+                    capitalize text-white mb-[3rem]
                     ">
                         join
                     </a>
