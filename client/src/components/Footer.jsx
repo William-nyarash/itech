c7c2d1a88ada0210effceccb1f5c3af90504301d
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-08-31 22:21:42 +0300

    increase font for the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer =()=> {
             <ul className="list-none  
             flex justify-center content-center gap-1.5
             bg-[#141423] py-[1em] text-white
-            text-[0.5rem]
+            text-[1.5rem]
             ">
                 <li>&copy; 2025</li>
                 <li> created for techies by techies</li>
