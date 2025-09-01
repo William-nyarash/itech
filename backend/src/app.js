6f1d5d124dbd7eb564888b656984ef4b165c2fb2
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-09-01 15:42:07 +0300

    add morgan middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const UserRouter = require('./routes/User.routes');
 const AttendanceRouter = require('./routes/attendance.routes');
 const env = require('./config/env');
+
 
 const connectDB = require('./config/db');
 
@@ -11,6 +14,8 @@ require('dotenv').config();
 connectDB();
 
 const app = express()
+app.use(cors());
+app.use(morgan('tiny'));
 app.use(express.json())
 
 app.use('/itech-club', UserRouter);
