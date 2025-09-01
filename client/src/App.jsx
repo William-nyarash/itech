dddd3bb4d2b30ba8dacec5d4c51e9a6a8f05644e
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-09-01 16:21:33 +0300

    fix route issue and refactor the navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,22 @@
-import {
-	BrowserRouter as  Router,
-	Routes,
-	Route
+import { Routes, Route, Link } from "react-router-dom";
+import Navs from "./components/Navs";
+import Footer from "./components/Footer";
+import Form from "./components/Form";
+import Main from "./components/pages/Main";
+import Attendance from "./components/pages/Attendance";
 
-} from "react-router-dom"
-import Navs from "./components/Navs"
-import Footer from "./components/Footer"
-import Main from "./components/Main"
-import Attendance from "./components/Attendance"
-import Form from "./components/Form"
-const App =()=> {
+const App = () => {
   return (
-	<div className="h-screen"> 
-		<Router>
-			<Navs />
-			<Routes>
-				<Route path="/login" element={<Form />} />
-				<Route path="/" element={<Main />} />
-				<Route path="'attandance" element={<Attendance/>} />
-			</Routes>
-		<Footer />
-		</Router>
-	</div>
-)
-}
-export default App
+    <div className="h-screen">
+	<Navs />
+      <Routes>
+        <Route path="/login" element={<Form />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/attendance" element={<Attendance />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
