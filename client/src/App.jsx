322641e41c594c3717d8d8041c750613fecb5ee5
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-09-01 15:38:01 +0300

    add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
+import {
+	BrowserRouter as  Router,
+	Routes,
+	Route
+
+} from "react-router-dom"
 import Navs from "./components/Navs"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
+import Attendance from "./components/Attendance"
+import Form from "./components/Form"
 const App =()=> {
   return (
-	<> 
-		<Navs />
-		<Main />
+	<div className="h-screen"> 
+		<Router>
+			<Navs />
+			<Routes>
+				<Route path="/login" element={<Form />} />
+				<Route path="/" element={<Main />} />
+				<Route path="'attandance" element={<Attendance/>} />
+			</Routes>
 		<Footer />
-	</>
+		</Router>
+	</div>
 )
 }
 export default App
