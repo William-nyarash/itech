2f780f30b964cd4322da8abce3c254ebcb684b37
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-09-02 23:36:13 +0300

    git refactor the nav to the main component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,17 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Navs from "./components/Navs";
-import Footer from "./components/Footer";
 import Form from "./components/Form";
 import Main from "./components/pages/Main";
 import Attendance from "./components/pages/Attendance";
 
 const App = () => {
   return (
-    <div className="h-screen">
-	<Navs />
+    <div className="relative">
       <Routes>
         <Route path="/login" element={<Form />} />
         <Route path="/" element={<Main />} />
         <Route path="/attendance" element={<Attendance />} />
       </Routes>
-      <Footer />
     </div>
   );
 };
