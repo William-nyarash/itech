d65a19dfab98905975fdfb7ec23755b48fa9886d
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-09-03 15:45:51 +0300

    modify get single user handler to login user

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -12,34 +12,42 @@ const env = require('../config/env')
   }
   }
 
-const getSingleUser = async (req, res) => {
+const loginUser = async (req, res) => {
   try {
-    const { id } = req.params;
+    const {email, password } = req.params;
 
-    const userData = await User.findById(id); 
+    const user = await User.findOne({email}); 
 
-    if (!userData) {
+    if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    res.json(userData);
+    const userMatch = await  bcrypt.compare(password, user.password);
+    if(!userMatch) {
+      return res.status(400).json({message: "invalid credantials"});
+    }
+
+    const sessionToken = jwt.sign({id:id,role:role},env.JWT_KEY,{expiresIn:'1h'});
+      res.json({
+      message: "Login successful",
+      token,
+      user: { id: user.id, name: user.name, email: user.email, role: user.role }
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
-
  const createUsers= async (req, res) => {
    try {
     const { name,email,admissionNumber,password,role, club} = req.body;
-
-    // Check if user already exists
+    
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
     const salt = await bcrypt.genSalt(env.BCRYPT_SALT_ROUNDS);
     const hashedPassword = await bcrypt.hash(password, salt);
-    // Create new user
+    
     const user = new User({
       name,
       email,
@@ -85,7 +93,7 @@ const getSingleUser = async (req, res) => {
 };
 module.exports = {
     createUsers,
-    getSingleUser,
+    loginUser,
     getUsers,
     deleteUsers,
     updateUsers
